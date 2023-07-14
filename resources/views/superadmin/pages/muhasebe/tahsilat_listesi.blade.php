09780d7508f9d239b8b6e13f7e299af914d96257
yusufishakk <ysfishak14@gmail.com>
2023-07-14 11:24:43 +0300

    metis

--- resources/views/superadmin/pages/muhasebe/tahsilat_listesi.blade.php
+++ resources/views/superadmin/pages/muhasebe/tahsilat_listesi.blade.php
@@ -60,7 +60,7 @@
     <!-- JAVASCRIPT -->
     <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/metismenu/metisMenu2.min.js') }}"></script>
     <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
     <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
     <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
